bce7015615344832b431adcacfa3c14bceea78e0
HenryLimaBR <djfilipebr@gmail.com>
2021-01-28 02:01:48 -0300

    The Legend of Dev: A Fix to the Fix

--- pages/api/source.js
+++ pages/api/source.js
@@ -1,6 +1,7 @@
-import ytdl from 'ytdl';
+//import ytdl from 'ytdl';
+import ytdl from '../../ytdl-fix/index.js'; // Temporary Fix For YTDL
 
 export default async function source(req, res) {
 	const { id } = req.query;
-	return id && id.length > 0 ? (await ytdl.getInfo(id)).formats : res.json({ error: true });
+	return id && id.length > 0 ? res.json((await ytdl.getInfo(id)).formats) : res.json({ error: true });
 }
