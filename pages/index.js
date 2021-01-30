487c9156b7b5cea50073f711d7f83a86df60f393
HenryLimaBR <djfilipebr@gmail.com>
2021-01-30 04:14:01 -0300

    Design Proto

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import { useState, useEffect } from 'react';
+import Head from 'next/head';
+
+import style from '../styles/index.module.css';
+
 export default function Home() {
+	function handleinput(e) {
+		console.log(e);
+	}
+
 	return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
+		<>
+			<Head>
+				<title>Cookie Web Player</title>
+				<meta name="theme-color" content="#000000" />
+			</Head>
+
+			<div className={style.container}>
+				<input type='text' onInput={handleinput} className={style.searchbox} placeholder='Search Here' />
+			</div>
+		</>
 	);
 }
