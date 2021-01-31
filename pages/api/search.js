115d156780977495e2db15f949f53dd94867b88b
HenryLimaBR <djfilipebr@gmail.com>
2021-01-31 08:07:07 -0300

    Search Route Return Only Video

--- pages/api/search.js
+++ pages/api/search.js
@@ -2,5 +2,15 @@ import yts from 'yt-search';
 
 export default async function search(req, res) {
 	const { q } = req.query;
-	return q && q.length > 0 ? res.json((await yts(q)).all) : res.json({ error: true });
+	if (q && q.length > 0) {
+		const data = (await yts(q)).all;
+		return res.json(data.filter(i => {
+			if (i.type === 'video') return i;
+		}));
+	} else {
+		return res.json({
+			error: true,
+			message: 'Your Search Is Empty'
+		});
+  }
 }
