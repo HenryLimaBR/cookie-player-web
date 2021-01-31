c0e4abd86831fc3444af5566cd39a51b2d13135b
HenryLimaBR <djfilipebr@gmail.com>
2021-01-31 08:07:59 -0300

    Added Music Playing Funcionality

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,40 @@
 import { useState, useEffect } from 'react';
 import Head from 'next/head';
+import axios from 'axios';
 
-import style from '../styles/index.module.css';
+import index from '../styles/index.module.css';
+
+async function search(q) {
+	return (await axios.get(`/api/search?q=${q}`)).data;
+}
+
+async function audio(id) {
+	return (await axios.get(`/api/audio?id=${id}`)).data;
+}
 
 export default function Home() {
-	function handleinput(e) {
-		console.log(e);
+
+	const [searchItems, setSearchItems] = useState(null);
+	const [player, setPlayer] = useState(null);
+
+	useEffect(() => {
+		setPlayer(new Audio());
+	}, []);
+
+	async function handleUserAudio(data) {
+		const formats = await audio(data.url);
+		player.src = formats[0].url;
+		player.play();
+	}
+
+	async	function handleUserSearch(e) {
+		if (e.key === 'Enter') {
+			const items = await search(e.target.value);
+			const com = items.map(item => {
+				return <Item data={item} key={item.videoId} play={handleUserAudio} />;
+			});
+			setSearchItems(com);
+		}
 	}
 
 	return (
@@ -15,9 +44,31 @@ export default function Home() {
 				<meta name="theme-color" content="#000000" />
 			</Head>
 
-			<div className={style.container}>
-				<input type='text' onInput={handleinput} className={style.searchbox} placeholder='Search Here' />
+			<div className={index.container}>
+
+				<div className={index.search_box}>
+					<input className={index.search} type='text' onKeyPress={handleUserSearch} placeholder='Search Here' />
+				</div>
+
+				<ul className={index.list}>{ searchItems }</ul>
+
+				<div className={index.player}></div>
 			</div>
+
+			<script src="//cdn.jsdelivr.net/npm/eruda"></script>
+			<script>eruda.init();</script>
 		</>
+	);
+}
+
+function Item(props) {
+	function send() {
+		props.play(props.data);
+	}
+
+	return (
+		<li className={index.list_item} onClick={send}>
+			<img className={index.item_image} src={props.data.image} />
+		</li>
 	);
 }
