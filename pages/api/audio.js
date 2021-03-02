30c58082d7101600545e3e1f9ff15f242f0a0be5
HenryLimaBR <djfilipebr@gmail.com>
2021-03-02 00:40:04 -0300

    Fixed media failure; Api serves only the better audio format available

--- pages/api/audio.js
+++ pages/api/audio.js
@@ -1,11 +1,16 @@
 //import ytdl from 'ytdl';
 import ytdl from '../../ytdl-fix/index.js'; // Temporary Fix For YTDL
 
-function filter({ formats }) {
-	return formats.filter(s => { if (s.hasAudio && !s.hasVideo) return s });
-}
-
 export default async function source(req, res) {
 	const { id } = req.query;
-	return id && id.length > 0 ? res.json(filter(await ytdl.getInfo(id))) : res.json({ error: true });
+	if (id && id.length > 0) {
+		const info = await ytdl.getInfo(id);
+		const hqaudio = ytdl.chooseFormat(info.formats, {
+			quality: 'highestaudio',
+			filter: 'audioonly'
+		});
+		return res.json([hqaudio]);
+	} else {
+		res.json({ error: true });
+	}
 }
