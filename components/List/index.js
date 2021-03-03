560732d7af385fb845206c410f0425941ab22c25
HenryLimaBR <djfilipebr@gmail.com>
2021-03-03 09:41:44 -0300

    List Component Scroll-Up Automatically

--- components/List/index.js
+++ components/List/index.js
@@ -1,13 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import style from './style.module.css';
 
 import Item from '../Item';
 
 export default function List(props) {
+	const listbox = useRef(null);
 	const [items, setItems] = useState(null);
 
 function update({ data, setWait, wait, setCover }) {
 		console.log(`List: Updating ${data.length} Results!`);
+		listbox.current.scrollTop = 0;
  	  return data.map((item, index) => {
    	  return <Item data={item} index={index + 1} key={item.videoId} setWait={setWait} wait={wait} setCover={setCover} />;
    	});
@@ -17,6 +19,5 @@ function update({ data, setWait, wait, setCover }) {
 		props.data ? setItems(update(props)) : console.log('List: Skipping Update!');
 	}, [props.data]);
 
-	return <ul className={style.list}>{items}</ul>;
+	return <ul className={style.list} ref={listbox}>{items}</ul>;
 }
-
