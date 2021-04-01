b84c965288c3343f409b355f80ab9c1fb376172d
HenryLimaBR <djfilipebr@gmail.com>
2021-04-01 13:45:03 -0300

    PWA Test #2

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,19 +1,24 @@
-import '../styles/global.css'
 import Head from 'next/head'
+import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
 	return (
 	  <>
   	  <Head>
 	      <meta name='charset' content='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+        <meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no' />
+        <link rel="manifest" href="manifest.json" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="application-name" content="Cookie Player" />
+        <meta name="apple-mobile-web-app-title" content="Cookie Player" />
+        <meta name="theme-color" content="#111111" />
+        <meta name="msapplication-navbutton-color" content="#111111" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="msapplication-starturl" content="/" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/res/img/icon.png" />
+        <link rel="apple-touch-icon" type="image/png" sizes="192x192" href="/res/img/icon.png" />
         <title>Cookie Player Web</title>
-        <link rel='manifest' href='/manifest.json' />
-        <link
-          href='/res/img/icon.png'
-          rel='icon'
-          type='image/png'
-        />
         <meta name='theme-color' content='#111' />
 	    </Head>
 	    <Component {...pageProps} />
