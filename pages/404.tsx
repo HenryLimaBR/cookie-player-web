188bfd688d585a0d47f2f62042988b7b844b220a
HenryLimaBR <djfilipebr@gmail.com>
2021-05-09 13:41:29 -0300

    Added Custom 404 Page

---
+++ pages/404.tsx
@@ -0,0 +1,17 @@
+import { Component } from 'react'
+
+import style from '../styles/404.module.css'
+
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div className={style.container}>
+        <h1>Error 404 T-T</h1>
+        <h2 style={{ cursor: 'pointer' }}>Ay Man! R u trying to access something?</h2>
+      </div>
+    )
+  }
+}
+
+export default NotFound
