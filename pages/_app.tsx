95852403d991bc5ba01dd7d7289348ed5eb35761
HenryLimaBR <djfilipebr@gmail.com>
2021-05-27 20:19:38 -0300

    Optimized Anicons Loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,17 +4,18 @@ import '../styles/global.scss'
 
 const App = ({ Component, pageProps }: AppProps) => {
 	return (
-	  <>
-  	  <Head>
-	      <meta name='charset' content='utf-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no' />
-        <link rel="icon" type="image/png" sizes="192x192" href="/res/img/icon.png" />
-        <title>Cookie Player Web</title>
-        <meta name='theme-color' content='#111111' />
-	    </Head>
-	    <Component {...pageProps} />
-	  </>
-  )
+		<>
+			<Head>
+				<meta name='charset' content='utf-8' />
+				<meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no' />
+				<link rel="icon" type="image/png" sizes="192x192" href="/res/img/icon.png" />
+				<title>Cookie Player Web</title>
+				<meta name='theme-color' content='#44475a' />
+				<link rel='preload' href='/res/fonts/anicons/AniconsGX.ttf' as='font' crossOrigin='' />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default App
