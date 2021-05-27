e57e332522976e52cffa4369861161e0571494f4
HenryLimaBR <djfilipebr@gmail.com>
2021-05-27 07:08:05 -0300

    Fixed Image Align on Cover Service

--- services/cover.ts
+++ services/cover.ts
@@ -8,7 +8,7 @@ class Cover {
 	getSmallAxis({ width, height }: HTMLImageElement) {
 		return height < width ? height : width
 	}
-	
+
 	async process(url: string) {
 		if (this.url) URL.revokeObjectURL(this.url)
 		const img = new Image()
@@ -19,13 +19,13 @@ class Cover {
 		const smallAxisRes = this.getSmallAxis(img)
 		this.canvas!.width = smallAxisRes
 		this.canvas!.height = smallAxisRes
-		this.ctx!.translate(-this.canvas!.width * 0.25, 0)
+		this.ctx!.translate(-(img.width - this.canvas!.width) / 2, 0)
 		this.ctx!.fillRect(0, 0, this.canvas!.width, this.canvas!.height)
 		this.ctx!.drawImage(img, 0, 0)
 		this.url = this.canvas!.toDataURL('image/jpeg', 1)
 		return this.url
 	}
-	
+
 	init() {
 		this.canvas = document.createElement('canvas')
 		this.ctx = this.canvas.getContext('2d')
