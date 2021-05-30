a3865064557fd96ee022700021428b4b9c95e1c3
HenryLimaBR <djfilipebr@gmail.com>
2021-05-30 06:57:15 -0300

    Cancel Support

--- services/api.ts
+++ services/api.ts
@@ -1,23 +1,37 @@
-import axios from 'axios'
+import axios, { Canceler } from 'axios'
 import type yts from 'yt-search'
 import type ytdl from 'ytdl-core'
 
 class Api {
-	baseURL = ''
+	private baseURL = ''
+	private audioCancel: Canceler | null = null
 
 	constructor(baseURL = '') {
 		this.baseURL = baseURL.charAt(baseURL.length - 1) === '/' ? baseURL.slice(0, baseURL.length - 1) : baseURL
 	}
 
-	async search(q: string) {
+	public async search(q: string) {
 		return (await axios.get(`${this.baseURL}/api/search?q=${q}`)).data as yts.VideoSearchResult[]
 	}
 
-	async audio(id: string) {
-		return (await axios.get(`${this.baseURL}/api/audio?id=${id}`)).data as ytdl.videoFormat[]
+	public async audio(id: string) {
+		if (this.audioCancel !== null) {
+			this.audioCancel()
+		}
+		try {
+			const response = await axios.get(`${this.baseURL}/api/audio?id=${id}`, {
+				cancelToken: new axios.CancelToken(e => {
+					this.audioCancel = e
+				})
+			})
+			this.audioCancel = null
+			return response.data as ytdl.videoFormat[]
+		} catch (err) {
+			return 'Audio Request was Canceled or Caught on Error'
+		}
 	}
 
-	cover(url: string) { return `${this.baseURL}/api/album?i=${url}` }
+	public cover(url: string) { return `${this.baseURL}/api/album?i=${url}` }
 }
 
 
