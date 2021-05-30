573e6ca3f3cd33f6afd52310ee2c2553157faae9
HenryLimaBR <djfilipebr@gmail.com>
2021-05-30 08:40:23 -0300

    Title Parser on Media Service

--- services/media.ts
+++ services/media.ts
@@ -2,12 +2,23 @@ import type yts from 'yt-search'
 import cover from './cover'
 
 class Media {
+	private parser(title: string) {
+		const separatedTitle = title.split(/-|~/)
+		const author = separatedTitle.splice(0, 1).join('-').trim()
+		const track = separatedTitle.join('').trim()
+		return {
+			author,
+			track
+		}
+	}
+
 	async set(data: yts.VideoSearchResult | yts.VideoMetadataResult) {
 		const trackImg = await cover.process(data.thumbnail)
+		const trackInfo = this.parser(data.title)
 		if ('mediaSession' in navigator) {
 			navigator.mediaSession!.metadata = new MediaMetadata({
-				title: data.title,
-				//artist: '',
+				title: trackInfo.track,
+				artist: trackInfo.author,
 				//album: '',
 				artwork: [
 					{ src: trackImg }
